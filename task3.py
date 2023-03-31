724bd241ceb873a0e5c132c0d219310dda25a23c
Alasdair-B <124393502+Alasdair-B@users.noreply.github.com>
2023-03-31 11:35:31 -0700

    commit on 'master'

--- task3.py
+++ task3.py
@@ -2,21 +2,33 @@
 
 import random
 
-Options = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
-Rock = [2, 3]
-Paper = [0, 4]
-Scissors = [1, 3]
-Lizard = [1, 4]
-Spock = [0, 2]
-Computer = None
+Options = ['Rock' , 'Paper', 'Scissors', 'Lizard', 'Spock']
+Pairs = {'Rock': [2, 3], 'Paper': [0, 4], 'Scissors': [1, 3], 'Lizard': [1, 4], 'Spock': [0, 2]}
 
-print("\nPrepare for a game of Rock Paper Scissors Lizard Spock\nYour options are as follows: ", Options)
+print("\nPrepare for a game of Rock Paper Scissors Lizard Spock\nYour options are as follows: ", end='')
+for i in Options:
+    print(f"{i}, ", end='')
+
+print('\n')
+for i in Options:
+    print(f"{i} will win against: {Options[Pairs[i][0]]} and {Options[Pairs[i][1]]}")
+
+
 
 def play():
-    Choice = str(input("Choose your weapon: "))
-    Computer = Options[random.randint(0,4)]
-    if Options.index(Computer)==Options.index(Choice):
-        print("Draw!\n")
-    elif Options.index(Computer) in 
+    Choice = str(input("\nChoose your weapon: "))
+    Computer = random.randint(0,4)
+    if Choice not in Options:
+        print("Invalid input")
+        play()
+    elif Computer==Options.index(Choice):
+        print(f"The computer chose {Options[Computer]}\nDraw!")
+        play()
+    elif Computer in Pairs[Choice]:
+        print(f"The computer chose {Options[Computer]}\nYou won!")
+        play()
+    else:
+        print(f"The computer chose {Options[Computer]}\nYou got beaten by the computer")
+        play()
 
 play()
---
+++ task4_problem1.py
@@ -0,0 +1,28 @@
+#! python3
+
+import random
+
+stats = {'Strength': [0], 'Dexterity': [1], 'Constitution': [2], 'Intelligence': [3], 'Wisdom': [4], 'Charisma': [5]}
+
+
+"""
+def Input():
+    Choice = input("Choose option a, b, or c:\na) roll 3 dice\nb) roll 4 dice, discard the lowest\nc) roll 3 dice, reroll 1s\n")
+    if Choice=='a':
+        A()
+    elif Choice=='b':
+        B()
+    elif Choice=='c':
+        C()
+    else:
+        print("Invalid input")
+        Input()
+
+def A():
+
+
+
+print("D&D character stat generator")
+Input()
+
+"""
