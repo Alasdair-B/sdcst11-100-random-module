65e84b5a2b9fedac4f745f30c505f10d24c0d2bb
Alasdair-B <124393502+Alasdair-B@users.noreply.github.com>
2023-04-03 11:35:50 -0700

    commit on 'master'

--- task4_problem1.py
+++ task4_problem1.py
@@ -2,10 +2,8 @@
 
 import random
 
-stats = {'Strength': [0], 'Dexterity': [1], 'Constitution': [2], 'Intelligence': [3], 'Wisdom': [4], 'Charisma': [5]}
+stats = [{'Strength': [0]}, {'Dexterity': [1]}, {'Constitution': [2]}, {'Intelligence': [3]}, {'Wisdom': [4]}, {'Charisma': [5]}]
 
-
-"""
 def Input():
     Choice = input("Choose option a, b, or c:\na) roll 3 dice\nb) roll 4 dice, discard the lowest\nc) roll 3 dice, reroll 1s\n")
     if Choice=='a':
@@ -19,10 +17,15 @@ def Input():
         Input()
 
 def A():
+    for i in stats:
+        a = random.randint(1,6)
+        b = random.randint(1,6)
+        c = random.randint(1,6)
+        n = str(stats[stats.index(i)].keys())-"dict_keys([''])"
+        print(n)
+
 
 
 
 print("D&D character stat generator")
 Input()
-
-"""
